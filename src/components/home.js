891518233adf72653847474764bed3ab8ad19587
anna-ostrovsky <annao@codevalue.net>
2018-12-29 17:47:44 +0200

    Added home page

--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,12 @@
 import React from 'react';
-//import Logo from './sidebar/logo';
+import home from './../images/home.jpg';
 
 const Home = () => {
 	return (
 		<div className="home">
 			<h3>Welcome to our store</h3>
 			<h3>to start shopping please log in</h3>
-			{/* <img src={Logo}></img> */}
+			<img src={home} alt="shop"></img>
 		</div>
 
 	);
